42457a7289771e7caab6c8d9a56edafa11de260b
reg <you@example.com>
2024-12-06 15:18:45 +0700

    авторизация

--- api/registrationUser.php
+++ api/registrationUser.php
@@ -56,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $formData['surname'],
             $formData['email'],
             $formData['phone'],
-           password_hash($formData['password'], PASSWORD_BCRYPT),
+           $formData['password'],
             $formData['agree'] ? 1:0,
         ]);
         $_SESSION['register-errors'] =[];
